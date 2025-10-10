9c999356a52755cafe44696d7b1aa64ce150075c
phuhoang06 <hoangvanphu2006@gmail.com>
2025-10-10 22:26:45 +0700

    v1.1

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn-media.sforum.vn',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   
